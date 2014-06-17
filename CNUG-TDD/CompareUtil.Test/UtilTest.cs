b636782a784f8bf5beb24114cb34cce6956c3c90
Mayank <MayankSri@Live.com>
2014-06-16 23:51:04 -0500

    Compare Util added, all green.

---
+++ CNUG-TDD/CompareUtil.Test/UtilTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CompareUtil.Test
+{
+    [TestClass]
+    public class UtilTest
+    {
+        Util _util = null;
+
+        [TestInitialize]
+        public void UtilTestSetup() {
+            _util = new Util();
+        }
+
+        [TestMethod]
+        [TestCategory("Unit")]
+        public void ComparingApplesAndOranges_ExpectingNotEqual() {
+
+            var result = _util.Compare("Apples", "Oranges");
+            Assert.AreEqual(result, "Not Equal");
+        }
+
+        [TestMethod]
+        [TestCategory("Unit")]
+        public void ComparingTwoAndTwo_ExpectingEqual() {
+
+            var result = _util.Compare(2, 2);
+            Assert.AreEqual(result, "Equal");
+        }
+
+        [TestMethod]
+        [TestCategory("Unit")]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void ComparingNullAndEmpty_ExpectingException() {
+
+            var result = _util.Compare(null as string, string.Empty);
+            Assert.AreEqual(result, false);
+        }
+    }
+}
---
+++ CNUG-TDD/CompareUtil/Util.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CompareUtil {
+
+    public class Util {
+        
+        public string Compare<T>(T a, T b) where T : IComparable {
+            return a.Equals(b) ? "Equal" : "Not Equal";
+        }
+    }
+}
